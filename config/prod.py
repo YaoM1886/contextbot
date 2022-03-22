7cd4acfaeec8b9ccddefc70a8bed06c8cdfba13b
YaoM1886 <yaom1886@gmail.com>
2022-03-22 15:50:37 +0100

    replaced postgres with postgresql in os.environ

--- config/prod.py
+++ config/prod.py
@@ -2,4 +2,6 @@ import os
 DEBUG = False
 SECRET_KEY=os.urandom(24)
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS=False
