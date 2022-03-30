0a8600b0cc2bc48cd226e85ce1ac62aed966019d
YaoM1886 <yaom1886@gmail.com>
2022-03-30 11:13:37 +0200

    fixed the prod mode

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import create_app, db
 # --------local testing----------------
 flask_app = create_app()
 if __name__ == "__main__":
-    with flask_app.app_context():
+    with flask_app.app_context().push():
         db.create_all()
     flask_app.run()
 
